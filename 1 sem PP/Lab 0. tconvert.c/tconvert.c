c90cdc2337ef149e9772ae1d9c8593b27cc32b06
Matvey <31614310+iNaHook@users.noreply.github.com>
2018-01-12 10:16:17 +0300

    Update tconvert.c

--- 1 sem PP/Lab 0. tconvert.c/tconvert.c
+++ 1 sem PP/Lab 0. tconvert.c/tconvert.c
@@ -1,37 +1,43 @@
-#define USAGE "Usage: tconvert TEMP [SCALE]\n\nWhere SCALE can be\n  C\tConvert from Celsius\n  F\tConvert from Fahrenheit\n  K\tConvert from Kelvin\n\nLeave blank to use all scales\n"
-
-
+#define zero "TEMP == null & SCALE == null;\n Notice Temp=numbers,Scale=C/K/F/NULL"
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
 
-float convert_c_f(float c){
+float convert_c_f(float c)
+{
 	return c * (9.0 / 5.0) + 32;
 }
 
-float convert_c_k(float c){
+float convert_c_k(float c)
+{
 	return c + 273.15;
 }
 
-float convert_f_c(float f){
+float convert_f_c(float f)
+{
 	return (f - 32) * (5.0 / 9.0);
 }
 
-float convert_k_c(float k){
-	return k - 273.15;
-}
-
-float convert_k_f(float k){
-	return k * (9.0 / 5.0) - 459.67;
-}
-
-float convert_f_k(float f){
+float convert_f_k(float f)
+{
 	return (f + 459.67) * (5.0 / 9.0);
 }
 
-int is_temperature_correct(float t, char c){
-	switch(c){
+float convert_k_c(float k)
+{
+	return k - 273.15;
+}
+
+float convert_k_f(float k)
+{
+	return k * (9.0 / 5.0) - 459.67;
+}
+
+int is_temperature_correct(float t, char c)
+{
+	switch(c)
+	{
 		case 'c':
 			if (t >= -273.15) return 1;
 			break;
@@ -47,41 +53,47 @@ int is_temperature_correct(float t, char c){
 	return 0;
 }
 
-void convert_from_c(float t){
+void convert_from_c(float t)
+{
 	if (! is_temperature_correct(t, 'c')) return;
 
 	printf("%.2f F\n%.2f K\n", convert_c_f(t), convert_c_k(t));
 }
 
-void convert_from_f(float t){
+void convert_from_f(float t)
+{
 	if (! is_temperature_correct(t, 'f')) return;
 
 	printf("%.2f C\n%.2f K\n", convert_f_c(t), convert_f_k(t));
 }
 
-void convert_from_k(float t){
+void convert_from_k(float t)
+{
 	if (! is_temperature_correct(t, 'k')) return;
 
 	printf("%.2f C\n%.2f F\n", convert_k_c(t), convert_k_f(t));
 }
 
-int wrong_scale_error(){
-	printf("Unknown scale! Should be one of C, F, K\n");
+int wrong_scale_error()
+{
+	printf("Wrong scale! Try again(C/F/K)\n");
 	return 1;
 }
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[])
+{
 	float input_temp;
 	char *input_scale_char;
 
-	if (argc < 2 || argc >  3){
-		printf(USAGE);
+	if (argc < 2 || argc >  3)
+	{
+		printf(zero);
 		return 0;
 	}
-
 	input_temp = atof(argv[1]);  /* 0.0 if number was not found */
 
-	if (argc == 2){
+	if (argc == 2)
+	{
 		printf(  "C: %.2f\n", input_temp);
 		convert_from_c(input_temp);
 		printf("\nF: %.2f\n", input_temp);
@@ -89,12 +101,17 @@ int main(int argc, char *argv[]){
 		printf("\nK: %.2f\n", input_temp);
 		convert_from_k(input_temp);
 	}
-	else if (argc == 3){
+	else if (argc == 3)
+	{
 		input_scale_char = argv[2];
 
-		if (strlen(input_scale_char) != 1) return wrong_scale_error();
+		if (strlen(input_scale_char) != 1) 
+		{
+			return wrong_scale_error();
+		}
 
-		switch(tolower(input_scale_char[0])){
+		switch(tolower(input_scale_char[0]))
+		{
 			case 'c':
 				convert_from_c(input_temp);
 				break;
@@ -105,7 +122,8 @@ int main(int argc, char *argv[]){
 				convert_from_k(input_temp);
 				break;
 
-			default: return wrong_scale_error();
+			default: 
+			return wrong_scale_error();
 		}
 	}
 	return 0;
