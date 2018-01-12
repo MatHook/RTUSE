0036838c5cdc49dc923b8780a634ded94b4054e9
Matvey <31614310+iNaHook@users.noreply.github.com>
2018-01-12 10:15:39 +0300

    Update main.c

--- 1 sem PP/Lab1.Чтение из файла/main.c
+++ 1 sem PP/Lab1.Чтение из файла/main.c
@@ -4,43 +4,40 @@
 
 int main(int argc, char *argv[])
 {
-	FILE *f = NULL;
+  FILE *f = NULL;
+  int c, i;
+  int   num;
+  char  s[256];
+  float fl;
 
-	int c, i;
+  if (argc > 1) 
+  {
+    f = fopen(argv[1], "rb");
+  }
+  else
+  {
+    char filename[256];
 
-	int   num;
-	char  s[256];
-	float fl;
+    printf("File name not found. Please, enter name: ");
+    scanf("%s", filename);
 
-	if (argc > 1)
-	{
-		f = fopen(argv[1], "rb");
-	}
-	else
-	{
-		char filename[256];
+    f = fopen(filename, "rb");
+  }
 
-		printf("Data file name not provided. Please, enter name: ");
-		scanf("%s", filename);
-		f = fopen(filename, "rb");
-	}
+  if (f == NULL)
+  {
+    printf("Error: file can't read!\n");
+    exit(1);
+  }
 
-	if (f == NULL)
-	{
-		printf("Error: file was not read!\n");
-		exit(1);
-	}
+  for (i = 0; (c = getc(f)) != -1; i++)
+  {
+    ungetc(c, f);
+    fread(&num, sizeof(int), 1, f);
+    fread(s, sizeof(char), 256, f);
+    fread(&fl, sizeof(float), 1, f);
 
-	for (i = 0; (c = getc(f)) != -1; i++)
-	{
-		ungetc(c, f);
-
-		fread(&num, sizeof(int), 1, f);
-		fread(s, sizeof(char), 256, f);
-		fread(&fl, sizeof(float), 1, f);
-
-		printf("Chunk %d:\n\t%d\n\t%s\n\t%f\n", i + 1, num, s, fl);
-	}
-
-	return 0;
+    printf("Pack %d:\n\t%d\n\t%s\n\t%f\n", i + 1, num, s, fl);
+  }
+  return 0;
 }
