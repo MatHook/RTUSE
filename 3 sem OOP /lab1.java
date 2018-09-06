9baa3df93466dfe2b645ccbc2f5e1b6587e8bc11
Matvey <mathook00@gmail.com>
2018-09-06 10:32:32 +0300

    Added comments

--- 3 sem OOP /lab1.java
+++ 3 sem OOP /lab1.java
@@ -10,17 +10,19 @@ public class lab1 {
         forefun(); //Sum of Array by for
         whilfun(); //Sum of Array by while
         dwhilfun(); //Sum of Array by do while
+        System.out.println("Output Arguments:");
         for (int i = 0; i < args.length; i++) //Args out
-            System.out.println(args[i]);
+            System.out.print(args[i] + " ");
         garmon(); // Harmonic output
         Randomizer3000(); //Random not sorted and sorted arrays
-        factorialfun(5);
+        factorialfun(5); //Factorial
     }
     public static void forefun() {
         int[] array = {1, 31, 87, 23};
         int sum = 0;
         for (int i = 0; i < array.length; i++)
             sum += array[i];
+        System.out.println("Sum by 'for' round:");
         System.out.println(sum);
     }
     public static void whilfun() {
@@ -28,6 +30,7 @@ public class lab1 {
         int sum = 0;
         for (int i = 0; i < array.length; i++)
             sum += array[i];
+        System.out.println("Sum by 'while' round:");
         System.out.println(sum);
     }
     public static void dwhilfun() {
@@ -35,10 +38,12 @@ public class lab1 {
         int sum = 0;
         for (int i = 0; i < array.length; i++)
             sum += array[i];
+        System.out.println("Sum by 'do while' round:");
         System.out.println(sum);
     }
     public static void garmon() {
         int[] array = new int[10];
+        System.out.println("\nHarmonical output:");
         for (int i = 0; i < array.length; i++) {
             System.out.println((float)1/(i+1));
         }
@@ -46,18 +51,20 @@ public class lab1 {
     public static void Randomizer3000() {
         Random rand = new Random();
         int[] array = new int[10];
+        System.out.println("UnSorted array:");
         for (int i = 0; i < array.length; i++) {
             array[i] = rand.nextInt();
-            System.out.println(array[i] + " ");
+            System.out.print(array[i] + " ");
         }
         Arrays.sort(array);
+        System.out.println("\nSorted array:");
         for (int i = 0; i < array.length; i++)
-            System.out.println(array[i] + " ");
+            System.out.print(array[i] + " ");
     }
     public static void factorialfun(int n) {
         int factor = 1;
         for (int i = 1; i <= n; i++)
             factor *= i;
-        System.out.println(factor);
+        System.out.print("\nFactorial of " + n +" is " + factor);
     }
 }
