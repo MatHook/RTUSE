8fa4a36e8812a53d2a6b14d3b499cc3bedd0d667
Matvey <mathook00@gmail.com>
2018-09-10 10:48:07 +0300

    Fix code style

---
+++ 3 sem OOP /S/A/D/PersonInfo.java
@@ -0,0 +1,40 @@
+package S.A.D;
+
+import java.util.Comparator;
+
+public class PersonInfo {
+    private String name;
+    private int tnumber;
+    private int dbirth;
+    public PersonInfo(int tnumber, String name, int dbirth) {
+        this.name = name;
+        this.tnumber = tnumber;
+        this.dbirth = dbirth;
+    }
+    public int getTnumber() {
+        return tnumber;
+    }
+    public void setTnumber(int tnumber) {
+        this.tnumber = tnumber;
+    }
+    public int getDbirth() {
+        return dbirth;
+    }
+    public void setDbirth(int dbirth) {
+        this.dbirth = dbirth;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public static Comparator<PersonInfo> PersonComparator = new Comparator<PersonInfo>() {
+        @Override
+        public int compare(PersonInfo s1, PersonInfo s2) {
+            String personName1 = s1.getName().toUpperCase();
+            String personName2 = s2.getName().toUpperCase();
+            return personName1.compareTo(personName2);
+        }
+    };
+}
--- 3 sem OOP /S/A/D/lab_11.java
+++ 3 sem OOP /S/A/D/lab_11.java
@@ -1,9 +1,33 @@
 package S.A.D;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.*;
+import java.util.Scanner;
 
 public class lab_11 {
-    public void main(){
+    public static void main(String[] args){
+        File file = new File("/Users/matvey/IdeaProjects/Labs/3 sem OOP /S/A/D/text.txt");
+        ArrayList<String> peoples = new ArrayList<String>();
+        Scanner sc = null;
 
+        try {
+            sc = new Scanner(file);
+
+            while(sc.hasNextLine()) {
+                String line = sc.nextLine().trim();
+                peoples.add(line);
+                System.out.println(peoples);
+            }
+        }
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        finally {
+            sc.close();
+        }
+        peoples.add(new PersonInfo(peoples.subList(0,10),peoples.subList(peoples.indexOf("|"), peoples.lastIndexOf("|"),peoples.subList(peoples.lastIndexOf("|"),peoples.toString().length()))));
     }
 }
