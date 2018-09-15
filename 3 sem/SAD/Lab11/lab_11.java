ebefcc015e87ef251f1a24c4e0606153caff80e7
Matvey <mathook00@gmail.com>
2018-09-15 13:36:30 +0300

    added 11 lab fix

--- 3 sem/SAD/Lab11/lab_11.java
+++ 3 sem/SAD/Lab11/lab_11.java
@@ -4,20 +4,23 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.*;
 import java.util.Scanner;
+import java.util.Comparator;
 
 public class lab_11 {
     public static void main(String[] args){
-        File file = new File("/Users/matvey/IdeaProjects/Labs/3 sem OOP /S/A/D/text.txt");
-        ArrayList<String> peoples = new ArrayList<String>();
+        File file = new File("/Users/matvey/IdeaProjects/Labs/3 sem/SAD/Lab11/text.txt");
+        ArrayList<PersonInfo> peoples = new ArrayList<PersonInfo>();
         Scanner sc = null;
 
         try {
             sc = new Scanner(file);
+            int i = 0;
 
             while(sc.hasNextLine()) {
-                String line = sc.nextLine().trim();
-                peoples.add(line);
+                String line = sc.nextLine();
+                peoples.add(new PersonInfo(line.substring(0, line.indexOf("|")), line.substring(line.indexOf("|"), line.lastIndexOf("|")), line.substring(line.lastIndexOf("|"), line.length())));
                 System.out.println(peoples);
+                i++;
             }
         }
         catch (FileNotFoundException e) {
@@ -26,6 +29,45 @@ public class lab_11 {
         finally {
             sc.close();
         }
-        peoples.add(new PersonInfo(peoples.subList(0,10),peoples.subList(peoples.indexOf("|"), peoples.lastIndexOf("|"),peoples.subList(peoples.lastIndexOf("|"),peoples.toString().length()))));
+        Collections.sort(peoples, PersonInfo.PersonComparator);
+        for (PersonInfo str: peoples) {
+            System.out.println(str);
+        }
     }
+}
+class PersonInfo {
+    public String name;
+    public String tnumber;
+    public String dbirth;
+    public PersonInfo(String tnumber, String name, String dbirth) {
+        this.name = name;
+        this.tnumber = tnumber;
+        this.dbirth = dbirth;
+    }
+    public String getTnumber() {
+        return tnumber;
+    }
+    public void setTnumber(String tnumber) {
+        this.tnumber = tnumber;
+    }
+    public String getDbirth() {
+        return dbirth;
+    }
+    public void setDbirth(String dbirth) {
+        this.dbirth = dbirth;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public static Comparator<PersonInfo> PersonComparator = new Comparator<PersonInfo>() {
+        @Override
+        public int compare(PersonInfo s1, PersonInfo s2) {
+            String personName1 = s1.getName().toUpperCase();
+            String personName2 = s2.getName().toUpperCase();
+            return personName1.compareTo(personName2);
+        }
+    };
 }
