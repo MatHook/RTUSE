91dc8df49ffcc2241341e8f1f779d455f6326422
iNaHook <mathook00@gmail.com>
2018-09-16 16:39:55 +0300

    Added 10 lab comments for debug

--- 3 sem/SAD/Lab10/lab_10.java
+++ 3 sem/SAD/Lab10/lab_10.java
@@ -5,12 +5,14 @@ import java.util.*;
 public class lab_10 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size of array: ");
         int N = sc.nextInt();
         int[] array = new int[N];
         for (int i = 0; i < N; i++) {
+            System.out.println("Enter " + (i + 1) + " element of array: ");
             array[i] = sc.nextInt();
         }
-        System.out.println(summ(array, 0));
+        System.out.println("Summ of elements array : " + summ(array, 0));
     }
     public static int summ(int[] n, int index) {
         if (index < n.length) {
