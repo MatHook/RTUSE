2f03be8e8cb32b4435da29c8f7fb4aaf6f24790f
iNaHook <mathook00@gmail.com>
2018-10-07 17:27:03 +0300

    Fixed test for 2 lab OOP 3 sem 2 Lab worked fine

--- 3 sem/OOPLabs/Lab2/CircleTest.java
+++ 3 sem/OOPLabs/Lab2/CircleTest.java
@@ -1,14 +1,11 @@
 package OOPLabs.Lab2;
 
-import OOPLabs.Lab2.Circle;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.runner.RunWith;
-
-import static org.junit.Assert.*;
 
 @RunWith(Arquillian.class)
 public class CircleTest {
