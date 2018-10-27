e872431b4675110302142d33ce9bac76840ec52d
Matvey <mathook00@gmail.com>
2018-10-27 17:18:06 +0300

    Added lab6_2

--- 3 sem/OOPLabs/Lab6/lab6_2.java
+++ 3 sem/OOPLabs/Lab6/lab6_2.java
@@ -2,6 +2,9 @@ package OOPLabs.Lab6;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.HashMap;
 
 public class lab6_2 {
     public static void main(String[] args) {
@@ -10,20 +13,58 @@ public class lab6_2 {
 }
 
 class Frame6_2 extends JFrame {
-    Frame6_2() {
-        JFrame frame6_2 = new JFrame("lab6_2");
-        JLabel north = new JLabel("North");
-        north.setLocation(300, 0);
-        JLabel south = new JLabel("South");
-        south.setLocation(300, 600);
-        JLabel east = new JLabel("East");
-        east.setLocation(600, 300);
-        JLabel west = new JLabel("West");
-        west.setLocation(0, 300);
+    HashMap<JLabel, String> notes;
 
-        frame6_2.setLayout(new FlowLayout());
-        frame6_2.setSize(600, 600);
-        frame6_2.setVisible(true);
-        frame6_2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    Frame6_2() {
+        super("Lab6_2");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(600, 600);
+
+        JLabel center = new JLabel("Center", JLabel.CENTER);
+        JLabel west = new JLabel("West", JLabel.CENTER);
+        JLabel east = new JLabel("East", JLabel.CENTER);
+        JLabel north = new JLabel("North", JLabel.CENTER);
+        JLabel south = new JLabel("South", JLabel.CENTER);
+
+        notes = new HashMap<JLabel, String>();
+
+        notes.put(center, "Welcome to Center");
+        notes.put(west, "Welcome to West");
+        notes.put(east, "Welcome to East");
+        notes.put(north, "Welcome to North");
+        notes.put(south, "Welcome to South");
+
+        MAdapter adapter = new MAdapter(this);
+
+        center.addMouseListener(adapter);
+        west.addMouseListener(adapter);
+        east.addMouseListener(adapter);
+        north.addMouseListener(adapter);
+        south.addMouseListener(adapter);
+
+        add(center, BorderLayout.CENTER);
+        add(west, BorderLayout.WEST);
+        add(east, BorderLayout.EAST);
+        add(north, BorderLayout.NORTH);
+        add(south, BorderLayout.SOUTH);
+
+        setVisible(true);
+    }
+}
+
+class MAdapter extends MouseAdapter {
+    Frame6_2 frame6_2;
+
+    public MAdapter(Frame6_2 frame6_2) {
+        super();
+
+        this.frame6_2 = frame6_2;
+    }
+
+    public void mouseEntered(MouseEvent e) {
+        if (!frame6_2.notes.containsKey(e.getSource())) {
+            return;
+        }
+        JOptionPane.showMessageDialog(null, frame6_2.notes.get(e.getSource()));
     }
 }
