7713590a725cd099bc2c9b0ea621eaab07e0c534
Matvey <mathook00@gmail.com>
2018-10-30 10:59:28 +0300

    Added lab8_1 Empty lab7_3

---
+++ 3 sem/OOPLabs/Lab7/lab7_3.java
@@ -0,0 +1,7 @@
+package OOPLabs.Lab7;
+
+public class lab7_3 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ 3 sem/OOPLabs/Lab8/lab8_1.java
@@ -0,0 +1,23 @@
+package OOPLabs.Lab8;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class lab8_1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String line;
+
+        System.out.print("Enter information ");
+        line = sc.nextLine();
+
+        try(FileWriter writer = new FileWriter("textfile", false)) {
+            writer.write(line);
+            writer.flush();
+        }
+        catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+}
