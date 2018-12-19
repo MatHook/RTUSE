f4c1c124a063d049586860a57f7d0f29cf65a93e
iNaHook <mathook00@gmail.com>
2018-12-19 21:40:42 +0300

    Fixes

--- 3 sem/OOPPractice/Prac5/prac5.java
+++ 3 sem/OOPPractice/Prac5/prac5.java
@@ -1,10 +1,17 @@
 package OOPPractice.Prac5;
 
+import java.util.Arrays;
+
 public class prac5 {
     public static void main(String[] args) {
+        int[] array = {1, 51, 13, 56, 2};
+        int low = 0;
+        int high = array.length - 1;
         //printN(3);
         //printNM(4);
-        printNAB(1,4);
+        System.out.println(Arrays.toString(array));
+        printRS(array, low, high);
+        System.out.println(Arrays.toString(array));
     }
 
     public static void printN(int n) {
@@ -25,14 +32,40 @@ public class prac5 {
             System.out.print(n + " ");
     }
 
-    public static void printNAB(int a, int b) {
-        if (a < b) {
-            for (int i = a; i <= b; i++)
-                System.out.println(i);
+    public static void printRS(int[] a, int l, int h) {
+        if (a.length == 0)
+            return;
+
+        if (l >= h)
+            return;
+
+        int middle = l + (h - l) / 2;
+        int state = a[middle];
+
+        int i = l, j = h;
+        while (i <= j) {
+            while (a[i] < state) {
+                i++;
+            }
+
+            while (a[j] > state) {
+                j--;
+            }
+
+            if (i <= j) {
+                int temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+                i++;
+                j--;
+            }
         }
-        else {
-            for (int i = b; i <= b; i++)
-                System.out.println(i);
-        }
+
+        if (l < j)
+            printRS(a, l, j);
+
+        if (h > i)
+            printRS(a, i, h);
     }
+
 }
